b52609e781a14e991fa390e4839ddb5339d0131a
Samit <samit7aug@gmail.com>
2025-11-01 11:45:16 +0530

    Update ProjectCard3.jsx

--- src/components/ProjectCard3.jsx
+++ src/components/ProjectCard3.jsx
@@ -11,7 +11,7 @@ const ProjectCard3 = () => {
   return (
     <div className="w-[40vw] max-w-md mx-auto max-md:w-[70vw] bg-[#111827] p-5 sm:p-6 rounded-2xl border border-purple-800 shadow-[0_0_25px_#9333ea55] hover:scale-[1.02] transition-all duration-300 ease-in-out text-white mb-8">
       
-      {/* Project Image */}
+     
       <div className="mb-4 sm:mb-6">
         <img
           src="Wanderlust.png"
@@ -20,17 +20,17 @@ const ProjectCard3 = () => {
         />
       </div>
 
-      {/* Title */}
+     
       <h2 className="text-xl sm:text-2xl font-bold mb-2">WanderLust</h2>
 
-      {/* Description */}
+      
       <p className="text-gray-300 text-sm sm:text-base mb-4 leading-relaxed">
         A travel listing platform where users can explore, create, and manage
         unique stay listings across the world. Built using Node.js, Express,
         MongoDB, and Bootstrap with dynamic authentication and image uploads.
       </p>
 
-      {/* Tech Stack */}
+      
       <div className="flex flex-wrap gap-2 mb-4">
         {techStack.map((tech, index) => (
           <span
@@ -42,7 +42,7 @@ const ProjectCard3 = () => {
         ))}
       </div>
 
-      {/* View Button */}
+      
       <div className="mt-3 font-medium px-5 py-2 rounded-full w-[50%] text-center m-auto bg-gray-800 hover:bg-gray-700 transition-all duration-300">
         <a href="https://project-vbic.onrender.com/listings" target="_blank" rel="noopener noreferrer">
           View Project
